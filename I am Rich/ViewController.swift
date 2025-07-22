acf10aa4d43b01ead1690ffe83102188b8b8a582
suleyilmaz22 <2303013099@student.beykent.edu.tr>
2025-07-22 23:10:04 +0300

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Şule Yılmaz on 22.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
